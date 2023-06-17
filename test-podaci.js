936a38023166e4ac413e82b40b6851cce4f913aa
MirnaKov <77199828+MirnaKov@users.noreply.github.com>
2023-06-18 01:04:36 +0200

    Delete test-podaci.js

--- test-podaci.js
+++
@@ -1,9 +0,0 @@
-import Pjesma from '../models/pjesma'
-
-export const PJESME = [
-  new Pjesma(0, 'Californication', 'Californication', '1999.', 'tekst'),
-  new Pjesma(1, 'Snow','Stadium Arcadium', '2006', 'tekst'),
-  new Pjesma(2, 'Dani California', 'Stadium Arcadium',  '2006', 'tekst'),
-  new Pjesma(3, 'Dark Necessities',  'The Getaway', '2016', 'tekst'),
-  new Pjesma(4, 'Cant Stop',  'By the Way', '2002.', 'tekst'),
-]
